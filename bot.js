c3ae21edda117ab588e31f4d16eed662e99090d6
Warrior-hound <45868404+Warrior-hound@users.noreply.github.com>
2018-12-15 20:29:38 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -136,6 +136,30 @@ client.on("message", async message => {
     message.channel.bulkDelete(fetched)
       .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
   }
+  bot.on('message', message => {
+    let messageArray = message.content.split(" ");
+    let command = messageArray[0];
+    let args = messageArray.slice(1);
+    if(command === settings.prefix + settings.rainbowcommand) {
+        const rolez = message.mentions.roles.first() || message.guild.roles.find(r=> r.name === args [0])
+        if(!rolez) return message.channel.send(settings.messageresponse.rolenotfound).catch(err=> message.channel.send("No response"))
+        if(!message.guild.member(bot.user.id).hasPermission("MANAGE_ROLES")) return message.channel.send(settings.messageresponse.missingperm).catch(err=> message.channel.send("no response"))
+        var colors = settings.rainbowrole
+        var rolestart = setInterval(function() {
+            var colorsz = colors[Math.floor(Math.random() * colors.length)];
+            rolez.setColor(colorsz)
+        }, settings.rainbowdelay); 
+            message.channel.send(settings.messageresponse.success).catch(err=> message.channel.send("No response"))
+
+    }
+    if(command === settings.prefix + settings.rainbowstop) {
+            setTimeout(function () {
+           process.exit()
+            }, 1000);
+           
+                       message.channel.send(settings.messageresponse.rainbowstop).catch(err=> message.channel.send("No response"))
+                       }
+});
 });
 
 client.login(config.token);
