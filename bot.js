c797a13a2aabd97701ee4973c7c77a12aa523e13
Warrior-hound <45868404+Warrior-hound@users.noreply.github.com>
2018-12-15 22:23:58 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -60,11 +60,13 @@ client.on("message", async message => {
   }
   
   if(command === "say") {
-     if(!message.member.roles.some(r=>["Administrator", "Moderator", "Staff", "HOUND", "MOD"].includes(r.name)) )
-      return message.reply("Sorry, you don't have permissions to use this!")
+     
     // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
     // To get the "message" itself we join the `args` back into a string with spaces: 
     const sayMessage = args.join(" ");
+    
+    if(!message.member.roles.some(r=>["Administrator", "Moderator", "Staff", "HOUND", "MOD"].includes(r.name)) )
+      return message.reply("Sorry, you don't have permissions to use this!")
     // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
     message.delete().catch(O_o=>{}); 
     // And we get the bot to say the thing: 
