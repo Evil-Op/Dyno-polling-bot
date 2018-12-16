f362b93a7334e14e2be922ef7a91c2a9cddc0180
Warrior-hound <45868404+Warrior-hound@users.noreply.github.com>
2018-12-16 17:47:28 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -140,6 +140,9 @@ client.on("message", async message => {
       .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
   }
   
+  if(command === `${prefix}hello`){
+    return message.channel.send("Hello");
+    
 });
 
 client.login(config.token);
