f8512f927a6cbe4351f407f6332c9dc06c7024e6
Warrior-hound <45868404+Warrior-hound@users.noreply.github.com>
2018-12-17 08:12:51 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -182,20 +182,7 @@ if(command === "report"){
     return;
   }
   
-  if(command === "botinfo"){
-
-    let bicon = bot.user.displayAvatarURL;
-    let botembed = new Discord.RichEmbed()
-    .setDescription("Bot Information")
-    .setColor("#15f153")
-    .setThumbnail(bicon)
-    .addField("Bot Name", bot.user.username)
-    .addField("Created On", bot.user.createdAt);
-
-    return message.channel.send(botembed);
-  }
-
-
+  
 });
 
 client.login(config.token);
