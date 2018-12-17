b2f9779dc470570e7ea59622f6e3e9a29180a6f9
hethtrhrtjhrjrjrjtkmggkg <45868404+Warrior-hound@users.noreply.github.com>
2018-12-17 20:19:13 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -169,45 +169,31 @@ client.on("message", async message => {
    return message.channel.send(botembed);
   }
   
-  if(command === "mute"){
-     if(!message.member.roles.some(r=>["Administrator", "Staff", "HOUND", "MOD"].includes(r.name)) )
-      return message.reply("Hey IMMORTAL, Sorry U cant do that!!");
-    let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    if(!tomute) return message.reply("Couldn't find user.");
-    let muterole = message.guild.roles.find(`name`, "muted");
-    //start of create role
-    if(!muterole){
-      try{
-        muterole = await message.guild.createRole({
-          name: "muted",
-          color: "#000000",
-          permissions:[]
-        })
-        message.guild.channels.forEach(async (channel, id) => {
-          await channel.overwritePermissions(muterole, {
-            SEND_MESSAGES: false,
-            ADD_REACTIONS: false
-          });
-        });
-      }catch(e){
-        console.log(e.stack);
-      }
-    }
-    //end of create role
-    let mutetime = args[1];
-    if(!mutetime) return message.reply("You didn't specify a time!");
-  
-    await(tomute.addRole(muterole.id));
-    message.reply(`<@${tomute.id}> has been muted for ${ms(ms(mutetime))}`);
-  
-    setTimeout(function(){
-      tomute.removeRole(muterole.id);
-      message.channel.send(`<@${tomute.id}> has been unmuted!`);
-    }, ms(mutetime));
-  }  
+  bot.on("message", async message => {
+    if(message.author.bot) return;
+    if(message.channel.type === "dm") return;
 
-  
-  
+    let prefix = config.prefix;
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1);
+
+    if (cmd === `${prefix}report`){
+
+        let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+        if(!rUser) return message.channel.send("Couldnt find the user");
+        let reason = args.join(" ").slice(22);
+    
+        let reportEmbed = new Discord.RichEmbed()
+        .setDescription("Reports")
+        .setColor("#15f153")
+        .addField("Reported User", `${rUser} with ID: ${rUser.id}`);
+    
+        message.channel.send(reportEmbed);
+        return;
+        
+    }
 });
+  
 
 client.login(config.token);
