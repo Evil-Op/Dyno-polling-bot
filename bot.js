57e7f727b082d05ab1bd7b7cb5609216df7d9d43
hethtrhrtjhrjrjrjtkmggkg <45868404+Warrior-hound@users.noreply.github.com>
2018-12-18 20:14:01 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -142,19 +142,42 @@ client.on("message", async message => {
       .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
   }
   
+  function checkBots(guild) {
+  let botCount = 0; // This is value that we will return
+  guild.members.forEach(member => { // We are executing this code for every user that is in guild
+    if(member.user.bot) botCount++; // If user is a bot, add 1 to botCount value
+  });
+  return botCount; // Return amount of bots
+}
+Okay, so now we will do same method, but to check how many humans are in guild
+
+function checkMembers(guild) {
+    let memberCount = 0;
+    guild.members.forEach(member => {
+      if(!member.user.bot) memberCount++; // If user isn't bot, add 1 to value. 
+    });
+    return memberCount;
+  }
+  
    if(command === "serverinfo"){
 
-    let sicon = message.guild.iconURL;
-    let serverembed = new Discord.RichEmbed()
-    .setDescription("Server Information")
-    .setColor("#15f153")
-    .setThumbnail(sicon)
-    .addField("Server Name", message.guild.name)
-    .addField("Created On", message.guild.createdAt)
-    .addField("You Joined", message.member.joinedAt)
-    .addField("Total Members", message.guild.memberCount);
+    .setAuthor(`${message.guild.name} - Informations`, message.guild.iconURL) // Will set text on top of embed to <guild name> - Informations, and will place guild icon next to it
+    .setColor('#f4df42') // Will set color of embed
+    .addField('Server owner', message.guild.owner, true) // Will add in-line field with server owner
+    .addField('Server region', message.guild.region, true) // Will add in-line field with server region
+    .addField('Channel count', message.guild.channels.size, true) // Will add in-line field with total channel count
+    .addField('Total member count', message.guild.memberCount) // Will add in-line field with total member count
+    // Now we will use our methods that we've created before
+    .addField('Humans', checkMembers(message.guild), true)
+    .addField('Bots', checkBots(message.guild), true)
+    // We also can add field with verification level of guild
+    .addField('Verification level', message.guild.verificationLevel, true)
+    // And now, we can finally add footer and timestamp
+    .setFooter('Guild created at:')
+    .setTimestamp(message.guild.createdAt); // Will set timestamp to date when guild was created
 
-    return message.channel.send(serverembed);
+    // And now we can send our embed
+    return message.channel.send(embed);
   }
 
   if(command === "botinfo"){
