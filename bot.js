f052dbf49ff3acaf353bf9476072b35938d00ba8
hethtrhrtjhrjrjrjtkmggkg <45868404+Warrior-hound@users.noreply.github.com>
2018-12-18 08:01:54 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 // Load up the discord.js library
 const Discord = require("discord.js");
+const ms = require("ms");
 
 // This is your client. Some people call it `bot`, some people call it `self`, 
 // some might call it `cootchie`. Either way, when you see `client.something`, or `bot.something`,
@@ -169,7 +170,7 @@ client.on("message", async message => {
    return message.channel.send(botembed);
   }
   
-    if (command === "report"){
+    if(command === "report"){
 
         let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
         if(!rUser) return message.channel.send("Couldnt find the user");
@@ -184,6 +185,30 @@ client.on("message", async message => {
         return;
         
     }
+   
+    if(command === "mute"){
+    let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!rUser) return message.channel.send("Couldn't find user.");
+    let rreason = args.join(" ").slice(22);
+
+    let reportEmbed = new Discord.RichEmbed()
+    .setDescription("Reports")
+    .setColor("#15f153")
+    .addField("Reported User", `${rUser} with ID: ${rUser.id}`)
+    .addField("Reported By", `${message.author} with ID: ${message.author.id}`)
+    .addField("Channel", message.channel)
+    .addField("Time", message.createdAt)
+    .addField("Reason", rreason);
+
+    let reportschannel = message.guild.channels.find(`name`, "reports");
+    if(!reportschannel) return message.channel.send("Couldn't find reports channel.");
+
+
+    message.delete().catch(O_o=>{});
+    reportschannel.send(reportEmbed);
+
+} 
+  
 });
   
 
