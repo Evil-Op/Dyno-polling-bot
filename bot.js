46644a82bbefefd59349d4273548671c3a66161e
hethtrhrtjhrjrjrjtkmggkg <45868404+Warrior-hound@users.noreply.github.com>
2018-12-18 19:04:50 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -229,6 +229,21 @@ client.on("message", async message => {
 //end of module
 }
   
+  
+ if(command === "whois"){
+  let user = message.mentions.users.first() || message.author;
+  let embed = new Discord.RichEmbed()
+  .setAuthor(`${user.tag}'s Info`, user.displayAvatarURL)
+  .setThumbnail(user.displayAvatarURL)
+  .setColor('RANDOM')
+  .addField('Username', user.username, true)
+  .addField('Discrim', user.discriminator, true)
+  .addField('Status', user.presence.status, true)
+  .addField('Bot?', user.bot, true)
+  .setThumbnail(user.displayAvatarURL)
+  message.channel.send(embed);
+}
+  
 });
   
 
