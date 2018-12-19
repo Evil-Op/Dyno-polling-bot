fa002f4a2975ce19f25c5e206bac01a14ef23475
hethtrhrtjhrjrjrjtkmggkg <45868404+Warrior-hound@users.noreply.github.com>
2018-12-19 14:49:34 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -343,11 +343,9 @@ function checkMembers(guild) {
 
 }
   
-  if (command == "nickname") 
-  if (message.content.includes('changeNick')){
-    if (!message.guild.me.hasPermission('MANAGE_NICKNAMES')) return message.channel.send('I don\'t have permission to change your nickname!');
-    message.member.setNickname(message.content.replace('changeNick ', ''));
-}
+  if (command === "changeNick") {
+    client.user.setUsername(args).then(user => message.reply(`My new nickname is ${user.username}!`)).catch(console.error);
+  }
   
 });
   
